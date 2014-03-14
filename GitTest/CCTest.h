748eb3a62ac3590973b7a8e99aa16c0ad8851564
Eric Wiener <ericw@urban-nutrition.com>
2014-03-14 14:58:56 -0700

    added test class

---
+++ GitTest/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  GitTest
+//
+//  Created by Eric W on 3/14/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ GitTest/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  GitTest
+//
+//  Created by Eric W on 3/14/14.
+//
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
